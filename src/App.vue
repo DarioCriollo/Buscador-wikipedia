095c2c361a0e71bbb65dea73ae4f794d06642a58
sagalu <sagalu.transport@gmail.com>
2021-11-15 16:21:40 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,9 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view/>
+    <div id="nav">
+      <!-- <router-link to="/">Search</router-link> | -->
+    </div>
   </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,79 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <br><br><br>
+    <HelloWorld msg="Bienvenido"/>
+
+  <div class="row justify-content-center">
+    <div class="col-4">
+      <img width="300px" alt="Vue logo" src="../assets/search.png">
+    </div>
+    <div class="col-4">
+      <br><br><br><br><br>
+      <div class="input-group mb-3">
+      <input type="text" class="form-control" placeholder="Digíte una palabra" v-model="word">
+      <div class="input-group-append">
+        <button class="btn btn-outline-success" v-on:click="searchEngine" >Buscar</button>
+      </div>
+    </div>
+
+    </div>
+  </div>
+  <br><br>
+   <div class="row justify-content-md-center">
+    <div class="col-md-auto">
+      
+      <div v-for="item in objects" :key="item">
+        <h3><span class="badge badge-success">{{item.pageid}} - {{item.title}}</span></h3>
+        <p><b>{{item.snippet}}</b></p>
+      </div>
+      <hr>
+    </div>
+
+  </div>
+
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import * as Vue from 'vue' // in Vue 3
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+
+  data(){
+    
+    return{
+      objects : [],
+      word : "",
+    }
+
+  },
+
+  methods:{
+    searchEngine: function(){
+
+      if(this.word.trim() == ""){
+        this.$swal('Error','Digíta una palabra!!!');
+        this.objects = []
+      }else{
+          const url = "https://en.wikipedia.org/w/api.php?action=query&list=search&srprop=snippet&format=json&origin=*&utf8=&srsearch="
+          axios.get(url+this.word).then(resp => {
+          console.log(resp.data.query.search);
+          this.objects = resp.data.query.search;
+          this.word = ""
+      });
+      }
+
+    }
   }
+
 }
 </script>
